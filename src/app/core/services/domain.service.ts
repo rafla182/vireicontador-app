420669e665f761849c769167a89538d1360f6150
rafaela.freire@peixeurbano.com <rafaela.freire@peixeurbano.com>
2020-02-17 10:08:58 -0300

    PROD

--- src/app/core/services/domain.service.ts
+++ src/app/core/services/domain.service.ts
@@ -9,7 +9,8 @@ export class DomainService {
 
         const domains = [
             { url: 'localhost', api: 'https://app-vireicontador.azurewebsites.net/api/' },
-            { url: 'vireicontador-app.azurewebsites.net', api: 'https://app-vireicontador.azurewebsites.net/api/' }
+            { url: 'vireicontador-app.azurewebsites.net', api: 'https://app-vireicontador.azurewebsites.net/api/' },
+            { url: 'app.vireicontador.com.br', api: 'https://app-vireicontador.azurewebsites.net/api/' }
         ];
 
         const domain = domains.find(t => t.url === document.location.hostname);
